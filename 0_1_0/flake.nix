c4c166bc60531615950f01ae6175cb02bcc95535
hugosenari <hugosenari@gmail.com>
2022-03-12 02:15:16 -0300

    chore: re index fastcgi

---
+++ 0_1_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''FastCGI library for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-fastcgi-0_1_0.flake = false;
+  inputs.src-fastcgi-0_1_0.owner = "ba0f3";
+  inputs.src-fastcgi-0_1_0.ref   = "refs/tags/0.1.0";
+  inputs.src-fastcgi-0_1_0.repo  = "fastcgi.nim";
+  inputs.src-fastcgi-0_1_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-fastcgi-0_1_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-fastcgi-0_1_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
